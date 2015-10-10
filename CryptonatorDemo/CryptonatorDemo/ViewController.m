41a825fbe4b4829c0718a77ac87b283ba633d33f
Derek316x <dereknetto@gmail.com>
2015-10-10 11:45:38 -0400

    added cryptonator folder

---
+++ CryptonatorDemo/CryptonatorDemo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  CryptonatorDemo
+//
+//  Created by Z on 10/10/15.
+//  Copyright © 2015 dereknetto. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+@end
