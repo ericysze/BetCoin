b4bdd6ce2ddab33e7fece4c26909fc493edf916d
Derek316x <dereknetto@gmail.com>
2015-10-13 00:50:38 -0400

    updated pods

---
+++ CryptonatorDemo/CryptonatorDemo/PasscodeViewController.h
@@ -0,0 +1,14 @@
+//
+//  PasscodeViewController.h
+//  CryptonatorDemo
+//
+//  Created by Z on 10/13/15.
+//  Copyright © 2015 dereknetto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PasscodeViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
+
+@end
---
+++ CryptonatorDemo/CryptonatorDemo/PasscodeViewController.m
@@ -0,0 +1,44 @@
+//
+//  PasscodeViewController.m
+//  CryptonatorDemo
+//
+//  Created by Z on 10/13/15.
+//  Copyright © 2015 dereknetto. All rights reserved.
+//
+
+#import "PasscodeViewController.h"
+#import "SWRevealViewController.h"
+
+@interface PasscodeViewController ()
+
+@end
+
+@implementation PasscodeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //setup RevealViewController functionality
+    if (self.revealViewController){
+        [self.sidebarButton setTarget: self.revealViewController];
+        [self.sidebarButton setAction: @selector(revealToggle:)];
+        [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
